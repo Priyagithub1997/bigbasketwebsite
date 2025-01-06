eebfeb7a0c936b046937d5b4a74f3c23a8596bdf
unknown <Priyanka>
2025-01-05 16:45:30 -1200

    changes in bestseller

---
+++ script.js
@@ -0,0 +1,38 @@
+var prev=document.getElementById("prev");
+var next=document.getElementById("next");
+var mysmartbasket1=document.getElementById("mysmartbasket-row-1");
+var mysmartbasket2=document.getElementById("mysmartbasket-row-2");
+var prevbestsellers=document.getElementById("prevbestsellers");
+var nextbestsellers=document.getElementById("nextbestsellers");
+var bestsellers1=document.getElementById("bestsellers-row-1");
+var bestsellers2=document.getElementById("bestsellers-row-2");
+
+next.addEventListener("click",function(){
+   
+    mysmartbasket1.style.display="none";
+    mysmartbasket2.style.display="block";
+    mysmartbasket2.style.display="flex";
+});
+
+
+prev.addEventListener("click",function(){
+    
+    mysmartbasket2.style.display="none";
+    mysmartbasket1.style.display="block";
+    mysmartbasket1.style.display="flex";
+  });
+
+  nextbestsellers.addEventListener("click",function(){
+   
+    bestsellers1.style.display="none";
+    bestsellers2.style.display="block";
+    bestsellers2.style.display="flex";
+});
+
+
+prevbestsellers.addEventListener("click",function(){
+    
+    bestsellers2.style.display="none";
+    bestsellers1.style.display="block";
+    bestsellers1.style.display="flex";
+  });
