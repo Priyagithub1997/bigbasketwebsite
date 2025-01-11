0881299f4e7d7369f72f6555323981a01961646b
unknown <Priyanka>
2025-01-10 19:10:40 -1200

    added changes

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
-var prev=document.getElementById("prev");
-var next=document.getElementById("next");
-var mysmartbasket1=document.getElementById("mysmartbasket-row-1");
-var mysmartbasket2=document.getElementById("mysmartbasket-row-2");
-var prevbestsellers=document.getElementById("prevbestsellers");
-var nextbestsellers=document.getElementById("nextbestsellers");
-var bestsellers1=document.getElementById("bestsellers-row-1");
-var bestsellers2=document.getElementById("bestsellers-row-2");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var mysmartbasket1 = document.getElementById("mysmartbasket-row-1");
+var mysmartbasket2 = document.getElementById("mysmartbasket-row-2");
+var prevbestsellers = document.getElementById("prevbestsellers");
+var nextbestsellers = document.getElementById("nextbestsellers");
+var bestsellers1 = document.getElementById("bestsellers-row-1");
+var bestsellers2 = document.getElementById("bestsellers-row-2");
 
 
-next.addEventListener("click",function(){
- 
-    mysmartbasket1.style.display="none";
-    mysmartbasket2.style.display="block";
-    mysmartbasket2.style.display="flex";
+next.addEventListener("click", function () {
+
+  mysmartbasket1.style.display = "none";
+  mysmartbasket2.style.display = "block";
+  mysmartbasket2.style.display = "flex";
 
 });
 
 
-prev.addEventListener("click",function(){
+prev.addEventListener("click", function () {
 
-    mysmartbasket2.style.display="none";
-    mysmartbasket1.style.display="block";
-    mysmartbasket1.style.display="flex";
-   
-  });
+  mysmartbasket2.style.display = "none";
+  mysmartbasket1.style.display = "block";
+  mysmartbasket1.style.display = "flex";
 
-  nextbestsellers.addEventListener("click",function(){
-   
-    bestsellers1.style.display="none";
-    bestsellers2.style.display="block";
-    bestsellers2.style.display="flex";
+});
+
+nextbestsellers.addEventListener("click", function () {
+
+  bestsellers1.style.display = "none";
+  bestsellers2.style.display = "block";
+  bestsellers2.style.display = "flex";
 });
 
 
-prevbestsellers.addEventListener("click",function(){
-    
-    bestsellers2.style.display="none";
-    bestsellers1.style.display="block";
-    bestsellers1.style.display="flex";
-  });
+prevbestsellers.addEventListener("click", function () {
 
-  var shopdd=document.getElementById("nav2-shopdd");
-  
-  var shopping=document.getElementById("shopping");
-  shopping.addEventListener("click",function(){
+  bestsellers2.style.display = "none";
+  bestsellers1.style.display = "block";
+  bestsellers1.style.display = "flex";
+});
 
-   shopdd.style.display="block";
-  });
+var shopdd = document.getElementById("nav2-shopdd");
 
-  var dropdown=document.getElementById("dropdown");
-  
-  var showdropdown=document.getElementById("showdropdown");
-  dropdown.addEventListener("click",function(){
+var shopping = document.getElementById("shopping");
+shopping.addEventListener("click", function () {
 
-   showdropdown.style.display="block";
-  });
+  shopdd.style.display = "block";
+  shopdd.style.display = "flex";
+});
+
+var dropdown = document.getElementById("dropdown");
+
+var showdropdown = document.getElementById("showdropdown");
+dropdown.addEventListener("click", function () {
+
+  showdropdown.style.display = "block";
+});
+
+
+var nav = document.getElementById("nav");
+var newnav = document.getElementById("newnav");
+window.onscroll = function () {
+
+  newnav.style.display = "block";
+  newnav.style.display = "flex";
+}
 
